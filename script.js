8c701c60eba0ed6ab9c2aaedd7929155c5e99bb3
Henrique Melachauscas <henriqueds13@hotmail.com>
2021-06-23 17:52:05 -0300

    Atualizando area 3

--- script.js
+++ script.js
@@ -35,3 +35,32 @@ document.querySelector('.slider--area').style.width =
         console.log(slideInfo);
     }
 
+    let n= "2";
+var el = document.getElementById('lista');
+el.addEventListener('click', function(e){
+    n = e.target.id;
+    switch(n){
+        case 'l1':
+            console.log('primeiro');
+            break;
+        case 'l2':
+            console.log('segundo');
+            break;
+        case 'l3':
+            console.log('terceiro');
+            break;
+        case 'l4':
+            console.log('quarto');
+            break;
+        case 'l5':
+            console.log('quinto');
+            break;
+        case 'l6':
+            console.log('sexto');
+            break;
+        case 'l7':
+            console.log('setimo');
+            break;
+    }
+});
+
